a19ea37b976807f218465d83f46ba76d2d7407a0
Ben Marley <benjaminjohnmarley@gmail.com>
2018-06-20 12:35:28 +0100

    change html

--- src/components/Emojis.js
+++ src/components/Emojis.js
@@ -3,9 +3,21 @@ import React from 'react';
 export default ({ emojiList }) => {
   return (
     <div className="emojiWrapper">
-      {emojiList.map(({ symbol }) => (
-        <span className="emojiSymbol">{symbol}</span>
+      {emojiList.map(({ symbol }, i) => (
+        <span key={i} className="emojiSymbol">
+          {symbol}
+        </span>
       ))}
     </div>
   );
 };
+
+// export default ({ emojiList }) => {
+//   return (
+//     <div className="emojiWrapper">
+//       {emojiList.map(({ symbol }) => (
+//         <span className="emojiSymbol">{symbol}</span>
+//       ))}
+//     </div>
+//   );
+// };
